bf3a5165bedd8c6a6a898b2beaf75cb6fedb8b00
Bailey Keefer <bnk423@gmail.com>
2020-03-12 16:38:29 -0400

    added wiki routes

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const { db, Page, User }   = require('./models');
+const wikiRouter = require('./routes/wiki');
+// const userRouter = require('./routes/user');
 
 db.authenticate().
 then(() => {
   console.log('connected to the database');
 })
 
-
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
+app.use('/wiki', wikiRouter);
+// app.use('/user', userRouter);
 
 app.get('/', (req, res) =>{
-  res.send('hi 2000');
+  res.send('hi /');
 })
 
 // take force off if deploying the app
