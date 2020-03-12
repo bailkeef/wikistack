2065caa6ce52f1ece55868e56ec35758ed2ddc8c
Bailey Keefer <bnk423@gmail.com>
2020-03-12 13:01:38 -0400

    finished step 1 Introduction and Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) =>{
+  res.send('hi 2000');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
+
