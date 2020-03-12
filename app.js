b90cbff641a77af7d1321b2dce8bc4dc3007b39b
Bailey Keefer <bnk423@gmail.com>
2020-03-12 15:47:29 -0400

    joined new team - syncing our models

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const { db, Page, User }   = require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -9,6 +16,14 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) =>{
   res.send('hi 2000');
 })
+
+// take force off if deploying the app
+const createModels = async () => {
+    await db.sync({force: true})
+
+}
+createModels()
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
